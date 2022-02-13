c311d6349a2591d9d02f6ae54092c072e861b214
Aidan Loten <asloten@gmail.com>
2022-02-13 18:20:03 -0500

    heroku deploy fix, comment out cors part 2

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const cors = require('cors');
 const path = require('path');
+// const cors = require('cors');
 
 const app = express();
 
 // Init middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json({ extended: false }));
 
 // Define routes
