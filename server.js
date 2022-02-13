5f7bda87e78f1b1730a184ffce57d30c01b3bd2a
Aidan Loten <asloten@gmail.com>
2022-02-13 17:50:13 -0500

    first attempt to deploy to Heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const PORT = 5000;
+const path = require('path');
+
+const app = express();
 
 // Init middleware
 app.use(cors());
@@ -10,4 +11,16 @@ app.use(express.json({ extended: false }));
 // Define routes
 app.use('/', require('./routes/router'));
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
