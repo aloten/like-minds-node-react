dbba62141414e2259e5da2fe083788b7287af8cd
Aidan Loten <asloten@gmail.com>
2022-02-13 22:43:45 -0500

    comment out cors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 
 // Init middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json({ extended: false }));
 
 // Define routes
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
 
